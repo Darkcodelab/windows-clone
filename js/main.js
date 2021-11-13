d675ab98f792b8756a0fa3f71365e10e8ab7e541
Darkcodelab <deepachakaravarthi434@gmail.com>
2021-11-14 01:32:27 +0530

    Added fn for fullscreen mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 let openedTrays = [];
 
+// Request FullScreen onLoad
+function requestFullScreen(element) {
+  // Supports most browsers and their versions.
+  var requestMethod =
+    element.requestFullScreen ||
+    element.webkitRequestFullScreen ||
+    element.mozRequestFullScreen ||
+    element.msRequestFullScreen;
+
+  if (requestMethod) {
+    // Native full screen.
+    requestMethod.call(element);
+  } else if (typeof window.ActiveXObject !== "undefined") {
+    // Older IE.
+    var wscript = new ActiveXObject("WScript.Shell");
+    if (wscript !== null) {
+      wscript.SendKeys("{F11}");
+    }
+  }
+}
+
+var elem = document.body; // Make the body go full screen.
+// TODO: Confirm FullScreen Mode with the user
+
+// Handle outside click (tray)
 window.addEventListener("click", (e) => {
   if (openedTrays.length > 0) {
     openedTrays.forEach((tray) => {
